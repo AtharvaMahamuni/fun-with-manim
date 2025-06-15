9b1735728497d9d9a4fff286c40be5dee7cad995
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2025-06-16 00:49:49 +0530

    new thing, skipping animations

--- ManimCE_YT/moving_objects/main.py
+++ ManimCE_YT/moving_objects/main.py
@@ -8,6 +8,8 @@ config.frame_width = 12.0
 
 class DefaultTemplate(Scene):
     def construct(self):
+        
+        self.next_section("Intro",skip_animations=True)
         axes = Axes(
             x_range=[-15, 15, 1],  # min, max, step
             y_range=[-8, 8, 1],
@@ -17,8 +19,12 @@ class DefaultTemplate(Scene):
         
         box = Rectangle(stroke_color=GREEN_C, stroke_opacity=0.7, fill_color=RED_B, fill_opacity=0.5, height=1, width=1)
         
+        self.next_section("Box")
         self.add(box)
         self.play(box.animate.shift(RIGHT*2), run_time=2)
+        
+        
+        self.next_section("Shfit", skip_animations=True)
         self.play(box.animate.shift(UP*3), run_time=2)
         self.play(box.animate.shift(DOWN*5+LEFT*5), run_time=2)
         self.play(box.animate.shift(UP*1.5+RIGHT*1), run_time=2)
